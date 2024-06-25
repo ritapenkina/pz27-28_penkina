5ca5e220ae9a8ee7c441318e151087583d0bd295
ritapenkina <rita.penkina.06@mail.ru>
2024-06-25 05:27:39 +0300

    Add files via upload

---
+++ zadanie3/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz3_penkina
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Tasks task = new Tasks();
+            task.name = textBox1.Text;
+            task.description = textBox2.Text;
+            task.date = textBox3.Text;
+            task.time = textBox4.Text;
+
+            string tasks = task.name + ", " + task.description + ", " + task.date + ", " + task.time;
+            listView1.Items.Add(tasks);
+        }
+
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            listView1.Items.Clear();
+        }
+    }
+    struct Tasks
+    {
+        public string name;
+        public string description;
+        public string date;
+        public string time;
+    }
+
+}
+    
